90248706759a301213325922d871c63a6f41a469
Albert <kamalov00albert@gmail.com>
2022-07-18 14:23:56 +0300

    Начальная сборка

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+import './build-in/lazyload';
+import detectTouch from './build-in/detectTouch';
+import setScrollbarWidth from './build-in/setScrollbarWidth';
+import anchorLinks from './build-in/anchorLinks';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import editableTextContainer from './custom/editableTextContainer';
+import initSliders from "./custom/initSliders";
+import fancybox from "./build-in/fancybox";
+import loadApi from "./build-in/loadApi";
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadApi();
+    detectTouch();
+    setScrollbarWidth();
+    anchorLinks();
+    fancybox();
+
+    // custom
+    initSliders();
+    editableTextContainer();
+});
+
+document.addEventListener('lazyloaded', () => {
+    ScrollTrigger.refresh();
+});
+
+window.addEventListener('load', function () {
+    header();
+    document.body.classList.add('loaded');
+    ScrollTrigger.refresh();
+    setTimeout(() => document.body.classList.add('animatable'), 300);
+});
