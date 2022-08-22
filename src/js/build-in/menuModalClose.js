779158494d276a40ad36e5bc96cd6363a3ffb20d
Albert <kamalov00albert@gmail.com>
2022-08-22 12:21:55 +0300

    Правки

--- src/js/build-in/menuModalClose.js
+++ src/js/build-in/menuModalClose.js
@@ -2,5 +2,7 @@ export default function menuModalClose() {
     const closeButton = document.querySelector('.modal-menu .modal__close');
     const menuModal = document.querySelector('.modal-menu .modal__inner');
 
-    closeButton.style.setProperty('--modal-height', menuModal.clientHeight + closeButton.clientHeight / 2 + 'px');
+    console.log(menuModal.offsetTop)
+
+    closeButton.style.setProperty('--modal-height', menuModal.offsetTop - closeButton.clientHeight + 'px');
 }
