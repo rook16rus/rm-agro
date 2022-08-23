71eb6ddf7fbac13f2da2fff021f3be222a301ec6
Albert <kamalov00albert@gmail.com>
2022-08-23 14:52:33 +0300

    Адаптировал модальное окно

--- src/js/build-in/gallerySlider.js
+++ src/js/build-in/gallerySlider.js
@@ -3,6 +3,8 @@ import {Swiper, Navigation, EffectFade, Autoplay, Pagination, HashNavigation, Gr
 Swiper.use([Navigation, EffectFade, Autoplay, Pagination, HashNavigation, Grid, FreeMode, Thumbs]);
 
 export default function gallerySlider() {
+    /* Инициализация слайдеров */
+
     const subSwiper = new Swiper('.modal-gallery__sub-slider', {
         slidesPerView: "auto",
         on: {
@@ -16,10 +18,35 @@ export default function gallerySlider() {
     })
 
     const swiper = new Swiper('.modal-gallery__slider', {
+        spaceBetween: 10,
         thumbs: {
             swiper: subSwiper
+        },
+        breakpoints: {
+            641: {
+                spaceBetween: 0
+            }
         }
+    })
 
+    /* Синхронизация слайдера в модальном окне с нажатой карточкой */
+
+    const images = document.querySelectorAll('.media__list--single .media__item');
+    images.forEach((img, index) => {
+        img.addEventListener('click', () => swiper.slideTo(index, 0))
+    })
+
+    /* Lazy load для iframe */
+
+    const iframes = document.querySelectorAll('.modal-gallery iframe')
+
+    swiper.on('slideChange', () => {
+        const currentIframe = swiper.slides[swiper.activeIndex].querySelector('iframe');
+
+        iframes.forEach(iframe => iframe.src = '')
+        if (currentIframe) {
+            currentIframe.src = 'https://www.youtube.com/embed/Z3TWuCm4ncI';
+        }
     })
 }
 
