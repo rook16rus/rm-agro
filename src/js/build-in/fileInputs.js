ab2af13ab3a80fa68b83aa6a8ecf02893d7673b6
Albert <kamalov00albert@gmail.com>
2022-08-24 15:18:45 +0300

    Адаптировал страницу vacancy

--- src/js/build-in/fileInputs.js
+++ src/js/build-in/fileInputs.js
@@ -5,8 +5,13 @@ export default function fileInputs() {
         input.addEventListener('change', function (e) {
             const inputContainer = input.closest('.js-input-file-container');
             const inputFileName = inputContainer.querySelector('.js-input-file-name');
+            const inputFileDesc = inputContainer.querySelector('.js-input-file-desc');
 
-            inputFileName.innerText = this.files[0].name;
+            if (matchMedia('(max-width: 640px)').matches) {
+                inputFileDesc.innerText = this.files[0].name;
+            } else {
+                inputFileName.innerText = this.files[0].name;
+            }
         });
     });
 }
