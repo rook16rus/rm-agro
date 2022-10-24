0d22680e2bd6f8421f4dd0d8bb7943ff1ffcaa56
Albert <kamalov00albert@gmail.com>
2022-10-24 12:15:48 +0300

    Добавил фансибокс для наград

--- src/js/build-in/fancybox.js
+++ src/js/build-in/fancybox.js
@@ -3,7 +3,10 @@ import { Fancybox } from '@fancyapps/ui';
 export default function fancybox() {
     Fancybox.bind('[data-fancybox]', {
         Image: {
-            zoom: false
+            zoom: false,
+            Panzoom: {
+                ratio: 3
+            },
         },
     })
 }
